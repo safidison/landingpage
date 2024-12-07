4a7eab1c3111f89e7f66afe02ad09dbad40614a2
RAKOTOVAO Safidison Jean <safidison.rj@gmail.com>
2024-12-07 09:24:52 +0300

    correction

--- resources/views/components/location-search.blade.php
+++ resources/views/components/location-search.blade.php
@@ -12,7 +12,7 @@
         <i class="fa fa-paper-plane text-[30px] sm:text-[18px] cursor-pointer hover:text-blue-500"></i>
     </div>
     <button type="submit"
-        class="ml-2 py-2 px-4 bg-[#36578A] !w-[100%] lg:w-[262px] lg:h-[91px] text-white text-sm font-semibold rounded-full hover:bg-blue-600 focus:outline-none sm:sm:col-span-4 sm:h-full">
+        class="ml-2 py-2 px-4 bg-[#36578A] !sm:w-[100%] lg:w-[262px] lg:h-[91px] text-white text-sm font-semibold rounded-full hover:bg-blue-600 focus:outline-none sm:sm:col-span-4 sm:h-full">
         <span class="sm:hidden"><x-typography type="button2">Rechercher</x-typography></span>
         <span class="lg:hidden"><x-typography type="body" color="text-white">Rechercher</x-typography></span>
     </button>
