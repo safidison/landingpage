07cb9cc5367246471fdb46fad6b91e3d4325805d
RAKOTOVAO Safidison Jean <safidison.rj@gmail.com>
2024-12-07 09:26:15 +0300

    correction

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,4 +1,4 @@
 @props(['class' => '', 'width' => '502px'])
-<button class="custom-btn w-[{{ $width }}] h-[80px] sm:h-[34px] !px-4 {{ $class }}">
+<button class="custom-btn w-[{{ $width }}] h-[80px] sm:h-[34px] lg:!px-4 sm:!px-2 {{ $class }}">
     {{ $slot }} <span class="fa fa-chevron-right text-white text-xl font-light"></span>
 </button>
